1a43f8d8a890148c08f2e004daa3596c87db8c98
Bern <103102485+NBernkastel@users.noreply.github.com>
2023-04-30 20:33:43 +0600

    458

--- tcp.py
+++ tcp.py
@@ -1,29 +1,42 @@
 import socket
 
-
-class UdpConnect:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+class TcpConnect:
+    client_sock = None
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
     is_server = False
     @classmethod
     def bind(cls, ip: str, port: int) -> None:
         """Work like a server, listen and get client socket"""
+        cls.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cls.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         cls.sock.bind((ip, port))
+        cls.sock.listen(1)
+        cls.client_sock, cls.adr = cls.sock.accept()
         cls.is_server = True
 
     @classmethod
     def connect(cls, ip: str, port: int) -> None:
         """Work like a client, will connect you to host"""
+        cls.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cls.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        cls.sock.connect((ip, port))
         cls.is_server = False
 
     @classmethod
     def getdata(cls) -> bytes:
-        data, address = cls.sock.recvfrom(32)
+        data = cls.client_sock.recv(32) if cls.is_server else cls.sock.recv(32)
         return data
 
     @classmethod
-    def senddata(cls, data: any , ip: str, port: int) -> None:
-        cls.sock.sendto(data, (ip,port))
+    def senddata(cls, data: any) -> None:
+        if cls.is_server:
+            cls.client_sock.send(data)
+        else:
+            cls.sock.send(data)
 
     @classmethod
     def close(cls) -> None:
+        if cls.is_server:
+            cls.client_sock.close()
         cls.sock.close()
